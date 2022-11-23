8f191b5c7defd1b0441843dcfc95b1aed97ec952
Romis <romis.nesmelov@gmail.com>
2022-11-23 10:29:41 +0300

    small refactoring

--- resources/views/documentations.blade.php
+++ resources/views/documentations.blade.php
@@ -5,7 +5,7 @@
     <div class="main internal">
         <div class="container">
             @include('blocks._breadcrumbs_block')
-            <div class="text_edit page_title">
+            <div class="text_edit page_title" style="min-height: 300px;">
                 <h1>{{ $head }}</h1>
                 <p><a href="{{ asset('statements/statement2018.pdf') }}" target="_blank">{{ trans('content.consolidated_statement').' 2018' }}</a></p>
                 <p><a href="{{ asset('statements/statement2020.pdf') }}" target="_blank">{{ trans('content.consolidated_statement').' 2020' }}</a></p>
