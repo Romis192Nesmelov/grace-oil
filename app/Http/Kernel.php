22adb258dc5b04f88c525a7ddc0154f5bd622126
Romis <romis.nesmelov@gmail.com>
2022-12-06 14:28:00 +0300

    disable base auth

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -14,8 +14,8 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $middleware = [
-        \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
-        \Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode::class,
+//        \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
+        \Illuminate\Foundation\Http\Middleware\CheckForMguaintenanceMode::class,
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
